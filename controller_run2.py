05315816fa41cb336b373a05e918f4cde5a2cb4f
antonlw <37929860+antonlw@users.noreply.github.com>
2024-01-12 14:17:49 +0000

    Update controller_run2.py

--- controller_run2.py
+++ controller_run2.py
@@ -23,6 +23,10 @@ def ax2speed(y):
         print("Y is " + str(y))
     return y*100
 
+def show_readiness()
+    fw.turn(60)
+    fw.turn(120)
+
 
 picar.setup()
 
@@ -30,36 +34,56 @@ fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
 fw.turning_max = 45
 
+running=0
 
-# Get a joystick
-with ControllerResource() as joystick:
-    # Loop until we're disconnected
-    while joystick.connected:
-        # read left x for turning angles
-        fw.turn(ax2angle(joystick.lx))
-        # read right y for running speed
-        speed=int(ax2speed(joystick.ry))
-        if speed>0:
-            bw.speed = speed
-            bw.forward()
-        elif speed<0:
-            bw.speed = 0-speed
-            bw.backward()
-        # This is an instance of approxeng.input.ButtonPresses
-        presses = joystick.check_presses()
-        if presses['circle']:
-            print('CIRCLE pressed since last check')
-        # We can also use attributes directly, and get at the presses object from the controller:
-        #if joystick.presses.circle:
-        #    print('CIRCLE pressed since last check')
-            print('Exit Mission.')
-            bw.stop()
-            break
-        # Or we can use the 'x in y' syntax:
-        #if 'triangle' in presses:
-        #    print('TRIANGLE pressed since last check')
-
-        # If we had any presses, print the list of pressed buttons by standard name
-        #if joystick.has_presses:
-        #    print(joystick.presses)
+while True:
+    # Get a joystick
+    with ControllerResource() as joystick:
+        # Loop until we're disconnected
+        while joystick.connected:
+            if running == 0:
+                while True:
+                    if presses['start']:
+                        print('START pressed, getting ready')
+                        show_readiness()
+                        running = 1
+                        break
+            else:
+                if presses['circle']:
+                    print('CIRCLE pressed since last check')
+                    print('Exit Mission.')
+                    bw.stop()
+                    running=0
+                    break
+            
+            # read left x for turning angles
+            fw.turn(ax2angle(joystick.lx))
+            # read right y for running speed
+            speed=int(ax2speed(joystick.ry))
+            
+            if speed>0:
+                bw.speed = speed
+                bw.forward()
+            elif speed<0:
+                bw.speed = 0-speed
+                bw.backward()
+            elif speed=0:
+                bw.stop()
+                
+            # This is an instance of approxeng.input.ButtonPresses
+            presses = joystick.check_presses()
+            #if presses['circle']:
+            #    print('CIRCLE pressed since last check')
+            # We can also use attributes directly, and get at the presses object from the controller:
+            #if joystick.presses.circle:
+            #    print('CIRCLE pressed since last check')
+            #    print('Exit Mission.')
+                
+            # Or we can use the 'x in y' syntax:
+            #if 'triangle' in presses:
+            #    print('TRIANGLE pressed since last check')
+    
+            # If we had any presses, print the list of pressed buttons by standard name
+            #if joystick.has_presses:
+            #    print(joystick.presses)
 
